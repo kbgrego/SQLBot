f527ef181ff0c0a8d67c6bb310c94fe614ed278c
Gregory <gregory.kabanov@gmail.com>
2016-09-02 14:46:14 +0300

    Add the main class of tester

--- src/com/Otium/SQLBotTester/Tester.java
+++ src/com/Otium/SQLBotTester/Tester.java
@@ -3,7 +3,7 @@ package com.Otium.SQLBotTester;
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(" SQLBot Tester ");
 
 	}
 
