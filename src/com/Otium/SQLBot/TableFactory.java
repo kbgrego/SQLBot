42d91039a44c2da5042d9528c0c2f890abf1600c
Gregory Kabanov <kb.grego@gmail.com>
2016-10-26 14:12:23 +0300

    Created Table Factory, dirty version

---
+++ src/com/Otium/SQLBot/TableFactory.java
@@ -0,0 +1,68 @@
+package com.Otium.SQLBot;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.Otium.SQLBot.Field.FIELD;
+import com.Otium.SQLBot.Record.Parameter;
+import com.Otium.SQLBot.Table.TABLE;
+
+/**
+ * Copyright 2016 Kabanov Gregory 
+ */
+
+public class TableFactory<T> {
+	private ConnectDatabase Connection;
+	private TABLE nameTable;
+	private Table Table;
+	private Class<T> MainClass;
+	
+
+	public TableFactory (ConnectDatabase Connection, Class<T> mainClass) {
+		this.Connection = Connection;			
+		this.MainClass = mainClass;
+		initialTable();
+	}
+	
+	private void initialTable(){
+		nameTable = new TABLE(MainClass.getName());
+		
+		Table = new Table(nameTable, Connection);
+		
+		initialFields();						
+		
+		Table.Create();
+	}
+
+	private void initialFields() {
+		Table.FieldsOfTable.add(new FIELD("rid"), FieldDataType.INTEGER, Key.PRIMARY, Increment.AUTO);
+				
+		for(java.lang.reflect.Field field : MainClass.getDeclaredFields())
+			if( field.isAnnotationPresent(TableField.class) )
+				Table.FieldsOfTable.add(new FIELD(field.getName()), FieldDataType.TEXT, Key.NONE, Increment.NOAUTO);
+	}
+	
+	public List<T> getList(){
+		List<T> list = new ArrayList<T>();
+		Class[] args = new Class[]{String.class};
+		
+		List<Record> data = Table.Select();
+		
+		for(Record row : data){
+			try {
+				T obj = MainClass.newInstance();
+				list.add(obj);
+				for(Parameter param : row.getParameters()){
+						Method seter = MainClass.getMethod("set" + param.Field.Name.toString(), args);
+						seter.invoke(obj, new String[]{param.Value});
+				}
+			} catch (Exception e) {
+				//silence is golden
+			}
+		}
+		
+		return list;
+	}
+
+}
---
+++ src/com/Otium/SQLBot/TableField.java
@@ -0,0 +1,13 @@
+package com.Otium.SQLBot;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Copyright 2016 Kabanov Gregory 
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TableField {
+
+}
