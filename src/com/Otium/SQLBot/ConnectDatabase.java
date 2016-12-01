4372a4a44bf00b58b8d48091553ea285605e7d64
Gregory Kabanov <kb.grego@gmail.com>
2016-12-01 16:03:34 +0200

    removed useless exception from debug

--- src/com/Otium/SQLBot/ConnectDatabase.java
+++ src/com/Otium/SQLBot/ConnectDatabase.java
@@ -107,7 +107,7 @@ public class ConnectDatabase{
 			return MainSQLConnection.createStatement()
 					                .executeQuery(query);
 		} catch (Exception e) {
-			if( DEBUG )
+			if( DEBUG && !e.getMessage().equals("query does not return ResultSet"))
 				System.err.println(e.getClass().getName() + ": " + e.getMessage());
 		}
 		return null;
